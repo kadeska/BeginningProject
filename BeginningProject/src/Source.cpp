da5a37c15daf40aed37b32da994d88746664a675
Joseph(kadeska) <kadeska23@gmail.com>
2021-12-13 16:56:03 -0800

    Implemented OpenGL
    Implemented OpenGL using VCPKG, and GLFW and GLEW. Draw a simple triangle on the screen. (still using Legacy OpenGL)

--- BeginningProject/src/Source.cpp
+++ BeginningProject/src/Source.cpp
@@ -1,4 +1,6 @@
-#include <GLFW/glfw3.h>
+#include <glew.h>
+#include <glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,11 +21,23 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "ERROR!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
