dd38ac23e43b0977f582de5b04cb966dc96ffad0
Joseph(kadeska) <kadeska23@gmail.com>
2021-12-21 21:17:09 -0800

    Introduction to shaders
    This is a very basic shader to make a red triangle draw to the screen. Previously was using "built in" shaders.

--- BeginningProject/src/Source.cpp
+++ BeginningProject/src/Source.cpp
@@ -2,6 +2,49 @@
 #include <glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    //error handling. 
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "!ERROR! : FAILED TO COMPILE SHADER!!!" << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    };
+
+    //All shaders compiled succesfully!
+    std::cout << "INFO : ALL SHADERS COMPILED SUCCESFULLY!" << std::endl;
+    return id;
+};
+
+static unsigned int CreatShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+};
+
 int main(void)
 {
     GLFWwindow* window;
@@ -29,8 +72,7 @@ int main(void)
     float positions[6] = {
         -0.5f, -0.5,
          0.0f, 0.5f,
-         0.5f, -0.5f
-    };
+         0.5f, -0.5f };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
@@ -40,6 +82,29 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    const std::string vs = R"glsl(
+    #version 330 core
+
+    layout(location = 0) in vec4 position;
+
+    void main(){
+       gl_Position = position;
+    }
+    )glsl";
+
+    std::string fs = R"glsl(
+        #version 330 core
+        
+        layout(location = 0) out vec4 color;
+        
+        void main()
+        {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+        )glsl";
+
+    unsigned int shader = CreatShader(vs, fs);
+    glUseProgram(shader);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -57,6 +122,10 @@ int main(void)
             glfwPollEvents();
         };
 
-    glfwTerminate();
-    return 0;
+        //is there any real differance in these two? 
+        glDeleteProgram(shader);
+        //glDeleteShader(shader);
+
+        glfwTerminate();
+        return 0;
 }
