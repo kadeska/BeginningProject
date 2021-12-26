db698fa33f481f284409125afd7ccfd709a3448c
Joseph(kadeska) <kadeska23@gmail.com>
2021-12-26 15:39:41 -0800

    Added Index buffer to optimize for graphics drawing.

--- BeginningProject/src/Source.cpp
+++ BeginningProject/src/Source.cpp
@@ -104,18 +104,30 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
+    float positions[] = {
         -0.5f, -0.5,
-         0.0f, 0.5f,
-         0.5f, -0.5f };
+         0.5f, -0.5f,
+         0.5f, 0.5f,
+         -0.5f, 0.5f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     ShaderProgrameSource source = ParseShader("src/res/shaders/Basic.shader");
     //std::cout << "VERTEX" << std::endl;
@@ -132,7 +144,7 @@ int main(void)
             /* Render here */
             glClear(GL_COLOR_BUFFER_BIT);
 
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 
             /* Swap front and back buffers */
